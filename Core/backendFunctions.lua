dc892a3280c4a892d4fa239945be4be27a23f65a
ItsJustMeChris <Chris@chris.com>
2016-11-16 00:38:09 -0500

    Cleaning
    Cleaning 1

--- Core/backendFunctions.lua
+++ Core/backendFunctions.lua
@@ -1,3 +1,31 @@
+function sanaStart()
+	MainFrame = CreateFrame("FRAME", nil, UIParent)
+	MainFrame = SetScript("OnUpdate", sanaUpdate)
+end
+
+function sanaUpdate(self, elapsed)
+	sanaTick = sanaTick + elapsed
+	if SanaToggle and sanaTick <= .250 then
+		sanaRotationStart()
+		elseif sanaTick >= 1 then
+		sanaTick = 0
+	end
+end
+
+function sanaToggle()
+	if SanaToggle ~= true then
+		SanaToggle = true
+		sanaSay("Enabled")
+	else 
+		SanaToggle = false
+		sanaSay("Disabled")
+	end
+end
+
+function sanaSay(args)
+	print("|cffff69b4[Sana]|r", args)
+end
+
 function sanaGetHealth(target)
 	return math.floor((UnitHealth(target) / UnitHealthMax(target)) * 100)
 end
