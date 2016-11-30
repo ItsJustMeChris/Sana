5680c7ec10ff1ec79567cd18cd67598ead7da3d5
ItsJustMeChris <Chris@chris.com>
2016-11-30 11:28:35 -0500

    Update
    Fixed Dispelling return

--- UnitAuras/Dispellable.lua
+++ UnitAuras/Dispellable.lua
@@ -1,10 +1,11 @@
 function sanaDispellable(buffID, unit)
 	local debuff = select(3, GetSpellInfo(buffID))
-if sanaDeBuff(unit, buffID) then
-dispellType = select(4, sanaDeBuff(unit, debuff))
-if dispellType == "Curse" or "Magic" or "Poison" then
-  return true;
-end
-else return false;
-end
+    if sanaDeBuff(unit, buffID) then
+        local dispellType = select(4, sanaDeBuff(unit, debuff))
+            if dispellType == "Curse" or "Magic" or "Poison" then
+                return true;
+                    else
+                return false;
+            end
+    end
 end
